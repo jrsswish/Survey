93c40149352217fb0133902aa47516c895df9f2c
jrsswish <122935245+jrsswish@users.noreply.github.com>
2025-05-19 20:30:59 -0600

    printed new age mean when submitting new survey

--- survey.py
+++ survey.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import csv
+from models import mean_age
 
 app = Flask(__name__)
 fields = ['First Name', 'Last Name', 'Age']
@@ -20,6 +21,9 @@ def submit():
   fname = request.form.get("fname")
   lname = request.form.get("lname")
   age = request.form.get("age")
+
+  result = mean_age()
+  print(result)
   
   with open('data.csv', 'a', newline='') as csvfile:
     csvwriter = csv.writer(csvfile)
