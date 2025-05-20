9f302380a6f105e01d562041c181f6a9e12cdba5
jrsswish <122935245+jrsswish@users.noreply.github.com>
2025-05-19 20:30:21 -0600

    make function for age mean

--- models.py
+++ models.py
@@ -1,11 +1,13 @@
 import numpy as np
 import pandas as pd
 
-with open('data.csv', 'r') as infile:
-  df = pd.read_csv(infile)
 
-X = df[['Age']]
 
-age_mean = np.mean(X)
+def mean_age():
+  with open('data.csv', 'r') as infile:
+    df = pd.read_csv(infile)
+    X = df[['Age']]
 
-print(age_mean)
+    age_mean = np.mean(X)
+
+    return age_mean
