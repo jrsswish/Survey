4a96279ef78cc6dfef64b91780dd3968cd8fa6dc
jrsswish <122935245+jrsswish@users.noreply.github.com>
2025-05-22 22:59:46 -0600

    added a graph for mean function

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-
+import matplotlib as plt 
+import mpld3
 
 
 def mean_age():
@@ -11,3 +12,16 @@ def mean_age():
     age_mean = np.mean(X)
 
     return age_mean
+  
+def graph():
+  # creates the plot
+  fig, ax = plt.subplots()
+  ax.plot(mean_age)
+  ax.set_title("Age mean")
+
+  # convert plot to html string
+  html_str = mpld3.fig_to_html(fig)
+
+  # save to html file
+  with open("stats.html", "w", encoding="utf-8") as f:
+    f.write(html_str)
