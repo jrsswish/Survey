bf1cad6bf35efa018a4f230e2c17f323094c7b8e
jrsswish <122935245+jrsswish@users.noreply.github.com>
2025-05-25 22:47:58 -0600

    implemented graph bar for gender count
    change gender_count function and implemented graph bar for gender_count

--- models.py
+++ models.py
@@ -16,11 +16,15 @@ def mean_age():
 def gender_count():
   with open('data.csv', 'r') as infile:
     df = pd.read_csv(infile)
-    X = df[df['Gender'] == 'male'].value_counts
-    X2 = df[df['Gender'] == 'female'].value_counts
-    X3 = df[df['Gender'] == 'others'].value_counts
+    # get the count of each gender
+    gender_count = df['Gender'].value_counts()
+    # put the count of each gender into variable
+    # Note: added 0 to prevent NoneType error
+    male = gender_count.get('male', 0)
+    female = gender_count.get('female', 0)
+    other = gender_count.get('other', 0)
 
-    return X, X2, X3
+    return male, female, other
 
 def graph():
   # creates the plot
@@ -32,6 +36,23 @@ def graph():
   ax.plot([0, 1], [mean_val, mean_val], label=f"Mean Age: {mean_val:.2f}")  
   ax.set_title("Mean Age")
   ax.set_title("Age mean")
+
+  # convert plot to html string
+  html_str = mpld3.fig_to_html(fig)
+
+  return html_str
+
+def graph2():
+  # creates the plot
+  male, female, other = gender_count()  
+  
+
+  fig, ax = plt.subplots()
+
+  # creates line from 0-1 x value and mean value-mean value y value
+  ax.bar(['male', 'female', 'other'], [male, female, other], color=['blue', 'pink', 'red'] )  
+  ax.set_title("Gender Count")
+  ax.set_ylabel("Count")
 
   # convert plot to html string
   html_str = mpld3.fig_to_html(fig)
